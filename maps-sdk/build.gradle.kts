14cb6570b7915bce00746775de6da4b114191eb3
songdingmao <songdingmao@yy.com>
2026-01-04 11:59:07 +0800

    fix(lib_mapbox：11.17.1-SNAPSHOT11): 让卫星图和离线loadStyle同时支持
    [Description] : 让卫星图和离线loadStyle同时支持
    [Reference]
    [Company] XAG-极飞科技
    [Team] Android Team

--- maps-sdk/build.gradle.kts
+++ maps-sdk/build.gradle.kts
@@ -143,7 +143,7 @@ afterEvaluate {
         
         groupId = "com.mapbox.maps"
         artifactId = "android-xag"
-        version = "11.17.1-SNAPSHOT9"
+        version = "11.17.1-SNAPSHOT11"
         
         pom {
           name.set("Mapbox Maps SDK for Android")
