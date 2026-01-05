acf4e2a59bc2b800b0bf3a16e8b70174768c969f
songdingmao <songdingmao@yy.com>
2026-01-05 14:20:48 +0800

    fix(lib_mapbox：11.17.1-SNAPSHOT12): 发布12制品
    [Description] : 发布12制品
    [Reference]
    [Company] XAG-极飞科技
    [Team] Android Team

--- maps-sdk/build.gradle.kts
+++ maps-sdk/build.gradle.kts
@@ -143,7 +143,7 @@ afterEvaluate {
         
         groupId = "com.mapbox.maps"
         artifactId = "android-xag"
-        version = "11.17.1-SNAPSHOT11"
+        version = "11.17.1-SNAPSHOT12"
         
         pom {
           name.set("Mapbox Maps SDK for Android")
